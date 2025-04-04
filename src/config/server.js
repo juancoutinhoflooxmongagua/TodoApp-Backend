3361c3f721ada8acdc63453a506c54e55124d250
juancoutinhoflooxmongagua <juan.coutinho.flooxmongagua@gmail.com>
2025-04-04 10:37:50 -0300

    feat: add Cors

--- src/config/server.js
+++ src/config/server.js
@@ -1,11 +1,12 @@
 const port = 3003
 const bodyParser = require('body-parser')
 const express = require('express')
-
+const AllowCors = require('./cors')
 const server = express()
 
 server.use(bodyParser.urlencoded({ extended: true }))
 server.use(bodyParser.json())
+server.use(AllowCors)
 
 server.listen(port, function () {
     console.log(`eaeee is running in port ${port}`)
