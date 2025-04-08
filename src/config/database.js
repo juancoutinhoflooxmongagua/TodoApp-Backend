bad01c0a0c428ec61c4e3e1b94da8a981fdb8c8d
juancoutinhoflooxmongagua <juan.coutinho.flooxmongagua@gmail.com>
2025-04-08 12:04:02 -0300

    feat: update Backend and fix bugs

--- src/config/database.js
+++ src/config/database.js
@@ -1,5 +1,15 @@
-const mongoose = require('mongoose')
+const mongoose = require('mongoose');
 
-mongoose.Promise = global.Promise
+mongoose.Promise = global.Promise;
 
-module.exports = mongoose.connect('mongodb://localhost/todo')
+mongoose.connect('mongodb://127.0.0.1:27017/todo', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  })
+  
+.then(() => console.log('MongoDB conectado 🎉'))
+.catch(err => {
+  console.error('Erro ao conectar no MongoDB 💥');
+  console.error(err);
+  process.exit(1);
+});
